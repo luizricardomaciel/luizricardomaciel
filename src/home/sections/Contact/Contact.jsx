20e54b52f41c581d8757bb93671d4cb6e97c4af5
luizricardomaciel <arisdeck01@gmail.com>
2024-10-11 19:47:25 -0300

    fix: link de redrecionamento da home

--- src/home/sections/Contact/Contact.jsx
+++ src/home/sections/Contact/Contact.jsx
@@ -49,7 +49,7 @@ function Contact() {
         <input
           type="hidden"
           name="redirectTo"
-          value="http://localhost:5173/obrigado"
+          value="https://luizricardomaciel.vercel.app/obrigado"
         ></input>
       </form>
     </section>
